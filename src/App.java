bd641980c54542a1e6f3dafc187a06ca5bf04e33
github username <um_2003_8@hotmail.com>
2022-06-15 11:08:54 -0400

    print a integer value

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		//int stand for integer that holds a whole numbers. could be positive or negative.		
+		int aValue = 42;
+		
+		System.out.println(aValue);
+	}
+
+}
