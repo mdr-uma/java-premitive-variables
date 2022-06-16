9032daa65f4a86e40cbe6fdb770da2643c8b535e
github username <um_2003_8@hotmail.com>
2022-06-16 09:49:11 -0400

    add primitive type long

--- src/App.java
+++ src/App.java
@@ -5,9 +5,11 @@ public class App {
 		//int stand for integer that holds a whole numbers. could be positive or negative.		
 		int aValue = 42;
 		short aShort = 93;
+		long aLong = 8304;
 		
 		System.out.println(aValue);
 		System.out.println(aShort);
+		System.out.println(aLong);
 	}
 
 }
